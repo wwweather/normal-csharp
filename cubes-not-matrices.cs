1cfe54a04abc147543ae1ed8e123453e62058296
Ольга <o.v.neliubova@gmail.com>
2023-03-22 12:20:05 +0800

    Create cubes-not-matrices.cs
    Данный код считает кубы чисел от 1 до N по известному N.

---
+++ cubes-not-matrices.cs
@@ -0,0 +1,27 @@
+/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+int Prompt (string message)
+{
+    System.Console.Write(message)
+    system value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int num = Prompt("Введите натуральное число: "); 
+
+/* Сложная проверка числа 
+double eps = 1E-14; // Значение допустимой ошибки
+if (double.Abs(x1 % 1) < eps) */
+
+if (num % 1 == 0)
+while (ind <= num) 
+    Console.WriteLine(Pow(num, 3));
+else 
+    Console.WriteLine("Число не подходит для задачи.");
+    
+break
